26951be403a2678a6f8ef45f6ec5799e8f5967a8
Brodino <brodino96@gmail.com>
2024-12-15 17:44:36 +0100

    a

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {
         ["hacker"] = { -- minigames = "keypad", "voltage", "fingerprint"
             {
                 startingCoords = vec3(-184.1242, -1015.6642, 30.0710),
-                arrivingCoords = vec4(-180.4203, -1017.4935, 114.2164),
+                arrivingCoords = vec3(-180.4203, -1017.4935, 114.2164),
                 minigame = "fingerprint",
             }
         }
