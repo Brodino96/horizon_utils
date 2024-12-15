ee9bbe3897bf0d055c25bf1d552d02060e276491
Brodino <brodino96@gmail.com>
2024-12-15 16:37:55 +0100

    a

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ Config = {
             {
                 startingCoords = vec3(-184.1242, -1015.6642, 30.0710),
                 arrivingCoords = vec4(-180.4203, -1017.4935, 114.2164, 341.3030),
-                minigame = "keypad",
+                minigame = "fingerprint",
             }
         }
     }
